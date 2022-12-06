775caf235cb4a08334f21c9ccd11dde59a34a6d9
anton-hav <anton.havrilenia@hotmail.com>
2022-12-06 18:20:22 +0100

    fixs enum converter

--- WebAPI/ReadingList.Core/ReadingPriority.cs
+++ WebAPI/ReadingList.Core/ReadingPriority.cs
@@ -1,11 +1,15 @@
-﻿namespace ReadingList.Core;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
+namespace ReadingList.Core;
+
+[JsonConverter(typeof(StringEnumConverter))]
 public enum ReadingPriority
 {
-    Never,
-    Low,
-    Medium,
-    High,
-    Urgent,
-    RightNow
+    Never = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Urgent = 4,
+    RightNow = 5,
 }
--- WebAPI/ReadingList.Core/ReadingStatus.cs
+++ WebAPI/ReadingList.Core/ReadingStatus.cs
@@ -1,9 +1,16 @@
-﻿namespace ReadingList.Core;
+﻿//using System.Text.Json.Serialization;
+//using Newtonsoft.Json.Converters;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ReadingList.Core;
+
+[JsonConverter(typeof(StringEnumConverter))]
 public enum ReadingStatus
 {
-    Scheduled,
-    InProgress,
-    Completed,
-    Skipped
+    Scheduled = 0,
+    InProgress = 1,
+    Completed = 2,
+    Skipped = 3,
 }
